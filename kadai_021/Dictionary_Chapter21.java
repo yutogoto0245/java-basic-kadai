ae4d6e314a917777cfa65581358733bea5c03c1e
yutogoto0245 <167101175+yutogoto0245@users.noreply.github.com>
2024-06-07 22:59:20 +0900

    Add files via upload

--- kadai_021/Dictionary_Chapter21.java
+++ kadai_021/Dictionary_Chapter21.java
@@ -3,7 +3,8 @@ package kadai_021;
 import java.util.HashMap;
 
 public class Dictionary_Chapter21 {
-	public void getFruit(final String fruit) {
+	
+	public static HashMap<String, String> getFruit() {
 		//英単語の辞書として機能するHashMapを宣言
 		HashMap<String, String> fruitMap = new HashMap<String, String>();
 
@@ -19,15 +20,9 @@ public class Dictionary_Chapter21 {
 		fruitMap.put("muscat",  "マスカット");
 		fruitMap.put("cherry",  "さくらんぼ");
 		
-		//System.out.println(fruitMap.get(fruit));
+		//メソッド getFruit();を実行すればHashMapのfruitMapを取得できるようreturnを記載
+		return fruitMap;
 		
-		if (fruitMap.get(fruit) == null) {
-			System.out.println(fruit + "は辞書に存在しません");
-			System.out.println();
-		} else {
-			System.out.print(fruit + "の意味は" + fruitMap.get(fruit));
-			System.out.println();
-		}
 	}
 
 }
